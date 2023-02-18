1105b9f2066a949d297fcae6aac3de9d52d1fab3
1998 <106678831+1998ky262@users.noreply.github.com>
2023-02-18 18:21:37 +0900

    Add files via upload

---
+++ openpyxl_test3.py
@@ -0,0 +1,21 @@
+import openpyxl
+import random
+import time
+
+# 新しいExcelファイルを作成する
+workbook = openpyxl.Workbook()
+
+# デフォルトのSheet1を取得する
+sheet = workbook.active
+
+# セルに値を書き込む
+for i in range(100):
+    random_n = random.random()
+    cell = sheet.cell(row=i+1, column=1, value=random_n)
+    print(random_n)
+    time.sleep(0.06)
+    
+
+# Excelファイルを保存する
+workbook.save('example.xlsx')
+workbook.close()
